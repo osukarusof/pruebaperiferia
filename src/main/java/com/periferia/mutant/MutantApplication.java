85ecb4662b6ec99254d8bd6d782daeccbd508bfb
Oscar Sanchez <oscar.s.m0711@gmail.com>
2024-09-06 19:05:15 -0500

    chore: initial configuration

---
+++ src/main/java/com/periferia/mutant/MutantApplication.java
@@ -0,0 +1,13 @@
+package com.periferia.mutant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutantApplication.class, args);
+	}
+
+}
